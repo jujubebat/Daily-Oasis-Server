415f9b3d21e6358275a39e6dd074e85d3c4fd848
jujubebat <bbwwpark@naver.com>
2019-09-18 20:10:32 +0900

    -AWS 테스트용

--- main/cron.py
+++ main/cron.py
@@ -1,3 +1,1 @@
 
-def scheduler():
-    pass
--- main/views.py
+++ main/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import UserSerializer, UserSerializerWithToken, ActivitySerializer
 from .models import Activity, User_Preference, Preference, Activity_Preference
-#import pandas
+import pandas
 
 #데이터베이스로부터 데이터를 가져오고, 선언해둔 시리얼라이저를 통해 데이터를 직렬화해준다.
 class activity(APIView):
