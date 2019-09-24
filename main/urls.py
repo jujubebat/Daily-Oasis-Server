f170c8d5112c2fe3dffcf90e04f5ab0ad6cc8fc8
jujubebat <bbwwpark@naver.com>
2019-09-25 01:13:53 +0900

    -Signup 오류 수정

--- main/urls.py
+++ main/urls.py
@@ -4,18 +4,24 @@ from django.conf.urls import include
 from rest_framework_jwt.views import obtain_jwt_token
 from rest_framework_swagger.views import get_swagger_view
 from . import views
+from .views import  DoneQuest, ActivityList, CurrentQuest, FinishQuest, UserList, ActivityReview
 
 urlpatterns = [
     path('current_user', views.current_user),
     path('silk', include('silk.urls', namespace='silk')),
     path('test',views.test),
     path('login', obtain_jwt_token),
-    path('signup', views.UserList),
+    path('signup', UserList.as_view()),
     path('activityList', views.ActivityList),
     path('currentQuest', views.DoneQuest),
     path('doneQuest', views.CurrentQuest),
     path('finishQuest', views.FinishQuest),
+    path('activityReview', ActivityReview.as_view()), #엑티비티하고 엑티비티 리뷰들 제공
+    path('writeReview', views.WriteReview),  #리뷰작성
 ]
+
+#퀘스트 완료 -> 레벨, 칭호 로직 반영
+#댓글 기능 -> 레벨, 칭호, 로직 반영
 
 #디버그 툴 관련
 if settings.DEBUG and 'debug_toolbar' in settings.INSTALLED_APPS:
@@ -30,14 +36,11 @@ urlpatterns += [
     path('api_doc', schema_view)
 ]
 
-
 #DoneQuest 엑티비티를 넘겨줄지 아님 엑티비티 pk를 넘겨줄지 정해야함
 #메모
 #위치기반 추천기능 추가
 #Tmap 지오코딩
 #만들어야할 api 목록
-#퀘스트 완료 -> 레벨, 칭호 로직 반영
-#댓글 기능 -> 레벨, 칭호, 로직 반영
 
 
 
