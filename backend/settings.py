4a6dbd50d5f24df8bc07a2bcbca4d5b18ad9332a
jujubebat <bbwwpark@naver.com>
2019-09-26 12:06:20 +0900

    -디비 RDS로 교체

--- backend/settings.py
+++ backend/settings.py
@@ -76,29 +76,29 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 
-# #아마존 RDS
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'daily_oasis',
-#         'USER': 'root',
-#         'PASSWORD': '00000000',
-#         'HOST': 'dailyoasisbackend.cbzjw5cm6v1b.ap-northeast-2.rds.amazonaws.com',
-#         'PORT': '',
-#     }
-# }
-
-#로컬
+#아마존 RDS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'daily_oasis',
         'USER': 'root',
-        'PASSWORD': '0000',
-        'HOST': 'localhost',
+        'PASSWORD': '00000000',
+        'HOST': 'dailyoasisbackend.cbzjw5cm6v1b.ap-northeast-2.rds.amazonaws.com',
         'PORT': '',
     }
 }
+
+# #로컬
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'daily_oasis',
+#         'USER': 'root',
+#         'PASSWORD': '0000',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
